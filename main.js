aed296f4d2ffd7efac7287c887d99c747f2d5629
Meme Cake789 <memecake788@gmail.com>
2023-11-05 18:09:25 +0000

    Development 0.0.03

--- main.js
+++ main.js
@@ -1,36 +1,70 @@
-// Coding Train / Daniel Shiffman
-// 5.18 Matter.js tutorial 
+ // module aliases
+ var Engine = Matter.Engine,
+ Render = Matter.Render,
+ World = Matter.World,
+ Bodies = Matter.Bodies,
+ Body = Matter.Body;
 
-// https://www.youtube.com/watch?v=uITcoKpbQq4
+// create an engine
+var engine = Engine.create();
 
-// Note that the syntax has been updated to use object destructuring
-const { Engine, World, Bodies, Composite } = Matter;
+// create a renderer
+var render = Render.create({
+ element: document.body,
+ engine: engine
+});
 
-let engine;
-let world;
-let circles = [];
-let boundaries = [];
+// create two boxes and a ground
+var player = Bodies.rectangle(200, 200, 80, 80);
+var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
-function setup() {
-    createCanvas(400, 400);
-    // create an engine
-    engine = Engine.create();
-    world = engine.world;
-    boundaries.push(new Boundary(150, 100, width* 0.6, 20, 0.3));
-    boundaries.push(new Boundary(250, 300, width* 0.6, 20, -0.3));  
-}
-    
-function mouseDragged() {
-    circles.push(new Circle(mouseX, mouseY, random(5, 10)));
-}
 
-function draw() {
-    background(51);
-    Engine.update(engine);
-    for (let i = 0; i < circles.length; i++) {
-        circles[i].show();
-    }
-    for (let i = 0; i < boundaries.length; i++) {
-        boundaries[i].show();
-    }
-}
+
+// add all of the bodies to the world
+World.add(engine.world, [player, ground]);
+
+// run the engine
+Engine.run(engine);
+
+// run the renderer
+Render.run(render);
+
+// implement side scrolling
+Matter.Events.on(engine, 'afterUpdate', function() {
+ Render.lookAt(render, {
+     min: { x: player.position.x - 400, y: 0 },
+     max: { x: player.position.x + 400, y: 600 }
+ });
+});
+
+// add controls
+var isGrounded = false;
+
+window.addEventListener('keydown', function(event) {
+ switch (event.keyCode) {
+     case 37: // left
+         Body.setVelocity(player, {x: -5, y: player.velocity.y});
+         break;
+     case 39: // right
+         Body.setVelocity(player, {x: 5, y: player.velocity.y});
+         break;
+     case 38: // up
+         if (isGrounded) {
+             Body.applyForce(player, player.position, {x: 0, y: -0.3});
+             isGrounded = false;
+         }
+         break;
+ }
+});
+
+Matter.Events.on(engine, 'collisionStart', function(event) {
+ var pairs = event.pairs;
+
+ for (var i = 0; i < pairs.length; i++) {
+     var pair = pairs[i];
+
+     if (pair.bodyA === player && pair.bodyB === ground || pair.bodyA === ground && pair.bodyB === player) {
+         isGrounded = true;
+     }
+ }
+});
