1ec16f76ee0b7250cf7cac0b127c08d29728ac91
Meme Cake789 <memecake788@gmail.com>
2023-11-06 14:38:02 +0000

    Dev 0.0.04 ( Side Scrolling and Player Movement )

--- main.js
+++ main.js
@@ -18,10 +18,12 @@ var render = Render.create({
 var player = Bodies.rectangle(200, 200, 80, 80);
 var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
+var platform = Bodies.rectangle(400, 500, 300, 30, { isStatic: true });
+
 
 
 // add all of the bodies to the world
-World.add(engine.world, [player, ground]);
+World.add(engine.world, [player, ground, platform]);
 
 // run the engine
 Engine.run(engine);
@@ -54,8 +56,11 @@ window.addEventListener('keydown', function(event) {
              isGrounded = false;
          }
          break;
+
  }
 });
+
+// add collision events
 
 Matter.Events.on(engine, 'collisionStart', function(event) {
  var pairs = event.pairs;
@@ -63,7 +68,7 @@ Matter.Events.on(engine, 'collisionStart', function(event) {
  for (var i = 0; i < pairs.length; i++) {
      var pair = pairs[i];
 
-     if (pair.bodyA === player && pair.bodyB === ground || pair.bodyA === ground && pair.bodyB === player) {
+     if (pair.bodyA === player && pair.bodyB === ground || pair.bodyA === ground && pair.bodyB === player)  { // checks if player is colliding with the ground
          isGrounded = true;
      }
  }
