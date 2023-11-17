76b344b4f2b915a434536a812d0f4887968d6678
Mem3 <memecake788@gmail.com>
2023-11-16 20:18:54 -0600

    Update main.js

--- main.js
+++ main.js
@@ -20,11 +20,15 @@ var render = Render.create({
     height: window.innerHeight,
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     wireframes: false,
     showDebug: true,
     showVelocity: true,
 
     
+=======
+    showVelocties: true 
+>>>>>>> parent of 5eb75cf (Development 0.01.06 ( Colors and Debug ))
 =======
     showVelocties: true 
 >>>>>>> parent of 5eb75cf (Development 0.01.06 ( Colors and Debug ))
