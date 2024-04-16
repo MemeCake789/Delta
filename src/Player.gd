9c08a03f30e5c110784caadb1881ea2bb407462f
Mem3 <memecake788@gmail.com>
2024-04-15 21:33:23 -0500

    Updated Textures
    Updated the Player and Wall textures using the proto textures

--- src/Player.gd
+++ src/Player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 350.0
-@export var JUMP_VELOCITY = -400.0
+@export var speed = 500
+@export var jumpVelocity = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,16 +13,19 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jumpVelocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
+	
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed
+	)
 
 	move_and_slide()
