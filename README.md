a969573277fde954ef6b71c3da6ecd01104fe271
Mem3 <memecake788@gmail.com>
2024-04-16 21:11:10 -0500

    Better Player Movement and Tilemap
    Adjusted player setting and added a tilemap ( WIP )

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Delta
- A simple platfomer
+#Delta
+A simple platfomer
+
+##To-Do:
+- [ ] Create a simple pixel art tilesheet for faster run time and saved space
+- [ ] Create shooting system 
+- [ ] Add global 2D shadows
--- src/Player.gd
+++ src/Player.gd
@@ -1,10 +1,17 @@
 extends CharacterBody2D
 
-@export var speed:int = 500  # Max movement speed
-@export var jumpVelocity:int = -400
-@export var acceleration: float = 300  # Adjust this value to control acceleration speed
-@export var deceleration: float = 100  # Adjust this value to control deceleration speed
-@export var jumpBufferTime: float = 0.2  # Adjust this value to control jump buffer time
+@export var walkSpeed = 700
+@export var runSpeed = 1200
+@export var jumpVelocity:int = -1200
+@export var walkAcceleration: int = 1000                  # Adjust this value to control acceleration when walking
+@export var runAcceleration: int = 2000                   # Adjust this value to control acceleration when running
+@export var deceleration: int = 1500                      # Adjust this value to control deceleration walkSpeed
+@export var jumpBufferTime: float = 0.2                   # Adjust this value to control jump buffer time
+@export var directionChangeAccelerationWalk: int = 2000   # Adjust this value to control acceleration when changing direction while walking
+@export var directionChangeAccelerationRun: int = 3000    # Adjust this value to control acceleration when changing direction while running
+
+var curentSpeed = 0
+var previousDirection = 0 # Keep track of the previous direction
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -16,26 +23,50 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("jump"):
 		if is_on_floor():
 			velocity.y = jumpVelocity
 		else:
 			jumpBufferCountdown = jumpBufferTime
 
+	if Input.is_action_pressed("run"):
+		curentSpeed = runSpeed
+		deceleration = 2500
+	else:
+		curentSpeed = walkSpeed
+		deceleration = 1500
+
 	if is_on_floor() and jumpBufferCountdown > 0:
 		velocity.y = jumpVelocity
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
-		# Increase velocity towards max speed with acceleration
-		velocity.x = move_toward(velocity.x, direction * speed, acceleration * delta)
+		# Check if the direction has changed
+		if direction != sign(velocity.x):
+			# Increase velocity towards max walkSpeed with directionChangeAcceleration
+			if Input.is_action_pressed("run"):
+				velocity.x = move_toward(velocity.x, direction * curentSpeed, directionChangeAccelerationRun * delta)
+			else:
+				velocity.x = move_toward(velocity.x, direction * curentSpeed, directionChangeAccelerationWalk * delta)
+		else:
+			# Increase velocity towards max walkSpeed with acceleration
+			if Input.is_action_pressed("run"):
+				velocity.x = move_toward(velocity.x, direction * curentSpeed, runAcceleration * delta)
+			else:
+				velocity.x = move_toward(velocity.x, direction * curentSpeed, walkAcceleration * delta)
 	else:
 		# Decelerate towards zero with deceleration
 		velocity.x = move_toward(velocity.x, 0, deceleration * delta)
 
 	move_and_slide()
 
+	if global_position.y > 3000:
+		global_position.x = 0
+		global_position.y = 0
+
 	# Decrease the jump buffer countdown.
 	jumpBufferCountdown -= delta
+
+	# Update the previous direction
+	previousDirection = direction
